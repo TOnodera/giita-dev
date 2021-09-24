c5c1583df312ba6a37329ea53a12929ddd69bd83
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-09-25 07:05:26 +0900

    ベースは作成。viewをどんどんつくる。

---
+++ frontend/src/components/BaseComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <layout
+        :sidebarVisible="isSidebarVisible"
+        @show-sidebar="showSidebar"
+        @hide-sidebar="hideSidebar"
+    >
+        <template v-slot:main>
+            <router-view />
+        </template>
+    </layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import Layout from '@/components/Layout';
+export default defineComponent({
+    components: {
+        Layout,
+    },
+    data() {
+        return {
+            sidebarVisible: false,
+        };
+    },
+    computed: {
+        isSidebarVisible() {
+            return this.sidebarVisible;
+        },
+    },
+    methods: {
+        hideSidebar() {
+            this.sidebarVisible = false;
+        },
+        showSidebar() {
+            this.sidebarVisible = true;
+        },
+    },
+});
+</script>
