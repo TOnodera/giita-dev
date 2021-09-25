703d8120b7281e04fc9e50e46ab424380bf6bae7
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-09-25 20:04:36 +0900

    ドキュメント作成。

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,100 +1,10 @@
-<template>
-    <div class="parent">
-        <div class="child">
-            <div class="p-grid">
-                <div class="p-md-6 p-sm-12">
-                    <card class="card card-height">
-                        <template #subtitle>
-                            効率的に学習内容をアウトプット
-                            <div class="image-icon-wrapper">
-                                <i class="pi pi-users"></i>
-                            </div>
-                        </template>
-
-                        <template #content>
-                            <p class="text-left">
-                                giitaはgithubと連携してコミット内容をqiitaに簡単に投稿できるサービスです。
-                                コミットした内容をピックアップして学習内容を手軽にみんなとシェアしましょう。
-                            </p>
-                        </template>
-                    </card>
-                </div>
-                <div class="p-md-6 p-sm-12">
-                    <card class="card card-height">
-                        <template #subtitle>
-                            利用方法
-                            <div class="image-icon-wrapper">
-                                <i class="pi pi-chevron-circle-down"></i>
-                            </div>
-                        </template>
-                        <template #content>
-                            <p class="text-left">
-                                利用にあたってgithubとqiitaのアカウントが必要になります。予め準備して下さい。
-                            </p>
-                            <p>
-                                <Button
-                                    label="githubアカウントを取得する。"
-                                    class="register-button"
-                                    @click="toGithubAccountRegisterPage"
-                                ></Button>
-                            </p>
-                            <p>
-                                <Button
-                                    label="qiitaアカウントを取得する。"
-                                    class="register-button"
-                                    @click="toQiitaAccountRegisterPage"
-                                ></Button>
-                            </p>
-                        </template>
-                    </card>
-                </div>
-            </div>
-            <div class="p-grid">
-                <div class="p-col-12">
-                    <card class="card">
-                        <template #content>
-                            <p>
-                                githubとqiitaのアカウントをお持ちの場合はすぐにサービスをご利用いただけます。
-                            </p>
-                            <p>
-                                <Button
-                                    label="サービスを利用する。"
-                                    class="register-button"
-                                    @click="toQiitaAccountRegisterPage"
-                                ></Button>
-                            </p>
-                        </template>
-                    </card>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
+<template><</template>
 
 <script>
 import { defineComponent } from 'vue';
 import Card from 'primevue/card';
 import Button from 'primevue/button';
-export default defineComponent({
-    components: {
-        Button,
-        Card,
-    },
-    data() {
-        return {
-            githubAccountRegisterUrl: 'https://github.com/signup?source=login',
-            qiitaAccountRegisterUrl: 'https://qiita.com/signup?redirect_to=%2F',
-        };
-    },
-    methods: {
-        toGithubAccountRegisterPage() {
-            window.open(this.githubAccountRegisterUrl, '_blank');
-        },
-        toQiitaAccountRegisterPage() {
-            window.open(this.qiitaAccountRegisterUrl, '_blank');
-        },
-    },
-});
+export default defineComponent({});
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/views/qiita/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>test</div>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({});
-</script>
